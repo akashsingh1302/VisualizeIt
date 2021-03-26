29868fa5bc5521faba14e2a52ecbaa13c011b7fd
Akash Singh <akash.s.13201@gmail.com>
2021-03-26 21:05:15 +0530

    first commit

---
+++ js/timeout.js
@@ -0,0 +1,6 @@
+async function speedset(){
+    var speed = document.getElementById("speed").value;
+    var speeddelay = 404 - speed;
+
+    await new Promise(resolve => setTimeout(() => {resolve();},speeddelay));
+}
